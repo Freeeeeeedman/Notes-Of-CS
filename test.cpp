bb79cd2423bff2d92aea2169c280cf6fba6a967d
FREEEEEEEDMAN <sunzhihao1984@hotmail.com>
2022-02-18 23:57:22 +0800

    commit 2.18_2

--- test.cpp
+++ test.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
 
-Circle circle1;
-Circle circle2(2,4);
+//Circle circle1;
+//Circle circle2(2,4);
 cout<<circle1.returnX()<<endl;
 //double a=100;
 //circle1.setX(a);
