9de2c83eaa63d863a10924ed5e8f504b474040d7
Freeeeeeedman <sunzhihao1984@hotmail.com>
2022-05-01 21:45:02 +0800

    commit 5.1_1

--- 杂/背包问题.cpp
+++ 杂/背包问题.cpp
@@ -46,7 +46,28 @@ void bag01_1d_() {
     cout << dp[bagweight] <<endl;
 }
 
+void bagmuti_01() {
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    vector<int> nums = {2, 3, 2};
+    int bagWeight = 10;
+    for(int i = 0; i < nums.size(); i++) {
+        while(nums[i] > 1) {
+            weight.push_back(weight[i]);
+            value.push_back(value[i]);
+            nums[i]--;
+        }
+    }
+    vector<int> dp(bagWeight + 1, 0);
+    for(int i = 0; i < weight.size(); i++) {
+        for(int j = bagWeight; j >= weight[i]; j--) {
+            dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
+        }
+    }
+    cout << dp[bagWeight] <<endl;
+}
+
 
 int main() {
-    bag01_1d_();
+    bagmuti_01();
 }
