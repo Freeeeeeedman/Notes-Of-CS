8675865eef468993073b463e2ba7ddefa06c5a55
Freeeeeeedman <sunzhihao1984@hotmail.com>
2022-05-12 21:42:00 +0800

    commit 5.12_`

---
+++ ACM.cpp
@@ -0,0 +1,17 @@
+//TODO ACM模式的输入和输出的练习
+
+//! 定义时一定要先初始化
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char a[20], b[20];
+    string c;
+    cin >> a;
+    getchar();
+    getline(cin, c);  
+    cout << a << endl;
+    cout << c << endl; 
+}
