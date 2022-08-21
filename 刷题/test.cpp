6fd62c27e9acbf797106a103f89161a78030d3eb
Freeeeeeedman <sunzhihao1984@hotmail.com>
2022-08-21 16:54:08 +0800

    commit 8.21_1

--- 刷题/test.cpp
+++ 刷题/test.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
-#include <string>
-#include <bits/stdc++.h>
-using namespace std;
 
-
-int main() {
-    vector<int> vec {1, 2, 3, 4, 5};
-    for(int i = 0, j = vec.size() - 1; i < vec.size(), j >= 0; ++i, --j) {
-        cout << vec[i] << " " << vec[j] << endl;
-    }
-}
